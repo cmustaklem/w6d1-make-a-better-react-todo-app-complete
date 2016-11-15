e3b7f6704bd97cf9e0770ca649fc36071c492a2f
Charles Mustaklem <charles.mustaklem@gmail.com>
2016-11-15 17:04:48 -0500

    Updated Homework

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -8,6 +8,6 @@ const TodoItem = (props) => {
     <span className={props.item.done?'text-done':''}>{props.item.text}</span>
 </li>
 }
-//I rearranged list-group-item disabled':'list-group-item and saw that the todo list would be grayed out without clicking the checkbox. The onClick passes the value that is set with markDone within the Todos component. I added in the checkboxMargin class so the text would not butt up against the checkbox.
+//I rearranged list-group-item disabled':'list-group-item and saw that the todo list would be grayed out without clicking the checkbox. The onClick passes the value that is set with markDone within the Todos component. I added in the checkboxMargin class so the text would not butt up against the checkbox.'text-done' is a class I added to the css file and it slashes the text within that box. 
 
 export default TodoItem
--- js/todoApp.js
+++ js/todoApp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Todos from '../components/Todos'
-//we are using ReactDOM because the values within this global js file will be consistently updated and placed onto the HTML file.  
+//we are using ReactDOM because the values within this global js file will be consistently updated and placed onto the HTML file. It is rendering the Todos component. It is rendering within the #todoApp div.  
 
 function renderView() {
    ReactDOM.render(
